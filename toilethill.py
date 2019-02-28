b56ed2a359b6951031365fdbe00621fa769c28ab
HeathDRobertson <CodeHappens@ToiletHill.io>
2019-02-28 15:37:49 -0700

    The Project5 push begins

--- toilethill.py
+++ toilethill.py
@@ -4,11 +4,24 @@ from IPython.display import display, Markdown, HTML
 
 
 def auto_notebook_links(p, t):
+    """
+    A function to auto-generate links to Jupyter Notebooks in a directory.  
+    Links are formatted to accommodate Udacity lesson, and lesson solution 
+    notebooks. 
+
+    Parameters
+    ----------
+    p : str
+        The path to the directory containing notebooks.
+    t : str
+        The description of the notebooks, markdown H3 header.
+    """
     path = Path(p)
     ulo = '<ul>'
     ul_li = ''
     ulc = '</ul>'
-    style = 'background-color:#fefdf6;padding:10px;border:1px solid #b1b0a8;border-radius:5px;'
+    style = 'background-color:#f7f7f7;padding:10px;border:1px solid #cfcfcf; \
+    border-radius:4px;line-height:1.2em;'
 
     dir_contents = os.listdir(path)
     all_notebooks = [file for file in dir_contents if file.endswith('.ipynb')]
@@ -18,7 +31,7 @@ def auto_notebook_links(p, t):
     ]
     notebooks.sort()
 
-    display(Markdown(f'### {t}\n___'))
+    display(Markdown(f'### {t}'))
 
     for idx, file in enumerate(notebooks):
         file_name = file.split(".")[0]
@@ -28,7 +41,8 @@ def auto_notebook_links(p, t):
         solution_file = f'{file_name}_solution.ipynb'
 
         if solution_file in all_notebooks:
-            li = f'<li><a href="{file_path}" target="_blank">{title}</a> : <a href="{path / solution_file}">Solution</a></li>'
+            li = f'<li><a href="{file_path}" target="_blank">{title}</a> : \
+            <a href="{path / solution_file}" target="_blank">Solution</a></li>'
             ul_li += li
         else:
             li = f'<li><a href="{file_path}" target="_blank">{title}</a></li>'
